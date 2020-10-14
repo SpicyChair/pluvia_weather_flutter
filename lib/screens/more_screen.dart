859241e546f88e9173e4f93f726f678cd6e27bea
Ethan-Scott Hin <hyperethan405@gmail.com>
2020-10-14 15:47:53 +0100

    Update more_screen.dart

--- lib/screens/more_screen.dart
+++ lib/screens/more_screen.dart
@@ -61,219 +61,212 @@ class _MoreScreenState extends State<MoreScreen> {
         elevation: 0,
       ),
       body: Center(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: [
-            Container(
-              height: 125,
-              margin: EdgeInsets.symmetric(horizontal: 10, vertical: 10),
-              decoration: BoxDecoration(
-                borderRadius: kBorderRadius,
-                color: Colors.grey[900],
-              ),
-              child: Stack(
-                children: [
-                  Positioned(
-                    top: 25,
-                    left: 15,
-                    child: Text(
-                      "Pluvia Weather",
-                      style: TextStyle(
-                          fontWeight: FontWeight.w300,
-                          fontSize: 35,
-                          color: Colors.white),
-                    ),
-                  ),
-                  Positioned(
-                    top: 70,
-                    left: 16,
-                    child: Text(
-                      "Version $version",
-                      style: TextStyle(
-                          fontWeight: FontWeight.bold,
-                          fontSize: 18,
-                          color: Colors.white),
-                    ),
-                  ),
-                  Positioned(
-                    top: 22,
-                    right: 15,
-                    child: Container(
-                      height: 70,
-                      width: 70,
-                      decoration: BoxDecoration(
-                        image: DecorationImage(
-                          image: AssetImage("assets/pluvia.png"),
-                          fit: BoxFit.fill,
-                        ),
-                      ),
-                    ),
-                  ),
-                ],
-              ),
-            ),
-            Expanded(
-              child: Container(
-                margin: EdgeInsets.all(8.0),
-                child: ListView(
+        child: Container(
+          margin: EdgeInsets.all(8.0),
+          child: ListView(
+            children: [
+              Container(
+                height: 125,
+                margin: EdgeInsets.all(2),
+                decoration: BoxDecoration(
+                  borderRadius: kBorderRadius,
+                  color: Colors.grey[900],
+                ),
+                child: Stack(
                   children: [
-                    SizedBox(
-                      height: 80,
-                      child: Card(
-                        child: Center(
-                          child: SwitchListTile(
-                            title: Text(
-                              "Dark Mode",
-                              style: TextStyle(
-                                  color: ThemeColors.primaryTextColor()),
-                            ),
-                            value: useDarkMode ?? false,
-                            onChanged: (bool value) async {
-                              useDarkMode = true;
-                              ThemeColors.switchTheme(value);
-                              //restart the app to show theme changes
-                              Navigator.pushReplacement(
-                                context,
-                                MaterialPageRoute(
-                                  builder: (BuildContext context) {
-                                    return HomeScreen();
-                                  },
-                                ),
-                              );
-                            },
-                            secondary: Icon(
-                              Icons.lightbulb_outline,
-                              color: ThemeColors.secondaryTextColor(),
-                            ),
-                          ),
-                        ),
-                        color: ThemeColors.cardColor(),
-                        shape:
-                            RoundedRectangleBorder(borderRadius: kBorderRadius),
+                    Positioned(
+                      top: 25,
+                      left: 15,
+                      child: Text(
+                        "Pluvia Weather",
+                        style: TextStyle(
+                            fontWeight: FontWeight.w300,
+                            fontSize: 35,
+                            color: Colors.white),
                       ),
                     ),
-                    SizedBox(
-                      height: 80,
-                      child: Card(
-                        child: Center(
-                          child: SwitchListTile(
-                            title: Text(
-                              "Use Imperial Units",
-                              style: TextStyle(
-                                color: ThemeColors.primaryTextColor(),
-                              ),
-                            ),
-                            value: useImperial ?? false,
-                            onChanged: (bool value) async {
-                              SharedPrefs.setImperial(value);
-                              useImperial = value;
-                              setState(() {});
-                              Scaffold.of(context).showSnackBar(SnackBar(
-                                  content:
-                                      Text("Refresh forecast to see changes")));
-                            },
-                            secondary: Icon(
-                              Icons.thermostat_outlined,
-                              color: ThemeColors.secondaryTextColor(),
-                            ),
-                          ),
-                        ),
-                        color: ThemeColors.cardColor(),
-                        shape:
-                            RoundedRectangleBorder(borderRadius: kBorderRadius),
+                    Positioned(
+                      top: 70,
+                      left: 16,
+                      child: Text(
+                        "Version $version",
+                        style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            fontSize: 18,
+                            color: Colors.white),
                       ),
                     ),
-                    /*
-                    SizedBox(
-                      height: 80,
-                      child: Card(
-                        child: Center(
-                          child: ListTile(
-                            title: Text("Donate"),
-                            onTap: () {
-                            },
-                            leading: Icon(LineIcons.heart_o),
+                    Positioned(
+                      top: 22,
+                      right: 15,
+                      child: Container(
+                        height: 70,
+                        width: 70,
+                        decoration: BoxDecoration(
+                          image: DecorationImage(
+                            image: AssetImage("assets/pluvia.png"),
+                            fit: BoxFit.fill,
                           ),
                         ),
-                        shape:
-                        RoundedRectangleBorder(borderRadius: kBorderRadius),
-                      ),
-                    ),
-
-                     */
-
-                    SizedBox(
-                      height: 80,
-                      child: Card(
-                        child: Center(
-                          child: ListTile(
-                            title: Text(
-                              "About Pluvia Weather",
-                              style: TextStyle(
-                                color: ThemeColors.primaryTextColor(),
-                              ),
-                            ),
-                            onTap: () {
-                              showAboutDialog(
-                                context: context,
-                                applicationVersion: version,
-                                applicationLegalese:
-                                    "Pluvia Weather is completely free and open source, and is licensed under GPLv3.",
-                              );
-                            },
-                            leading: Icon(
-                              Icons.more_horiz_outlined,
-                              color: ThemeColors.secondaryTextColor(),
-                            ),
-                          ),
-                        ),
-                        shape:
-                            RoundedRectangleBorder(borderRadius: kBorderRadius),
-                        color: ThemeColors.cardColor(),
-                      ),
-                    ),
-                    SizedBox(
-                      height: 12,
-                    ),
-                    Container(
-                      height: 130,
-                      child: Column(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        crossAxisAlignment: CrossAxisAlignment.center,
-                        children: [
-                          FlatButton(
-                            onPressed: () async {
-                              const url =
-                                  "https://github.com/SpicyChair/pluvia_weather_flutter";
-                              if (await canLaunch(url)) {
-                                await launch(url);
-                              }
-                            },
-                            child: ThemeColors.isDark
-                                ? Image.asset(
-                                    "assets/GitHub-Mark-Light-64px.png")
-                                : Image.asset("assets/GitHub-Mark-64px.png"),
-                          ),
-                          SizedBox(
-                            height: 12,
-                          ),
-                          Center(
-                            child: Text(
-                              "View Pluvia Weather on Github",
-                              style: TextStyle(
-                                  fontSize: 14,
-                                  fontWeight: FontWeight.bold,
-                                  color: ThemeColors.primaryTextColor()),
-                            ),
-                          ),
-                        ],
                       ),
                     ),
                   ],
                 ),
               ),
-            ),
-          ],
+              SizedBox(
+                height: 80,
+                child: Card(
+                  child: Center(
+                    child: SwitchListTile(
+                      title: Text(
+                        "Dark Mode",
+                        style: TextStyle(
+                            color: ThemeColors.primaryTextColor()),
+                      ),
+                      value: useDarkMode ?? false,
+                      onChanged: (bool value) async {
+                        useDarkMode = true;
+                        ThemeColors.switchTheme(value);
+                        //restart the app to show theme changes
+                        Navigator.pushReplacement(
+                          context,
+                          MaterialPageRoute(
+                            builder: (BuildContext context) {
+                              return HomeScreen();
+                            },
+                          ),
+                        );
+                      },
+                      secondary: Icon(
+                        Icons.lightbulb_outline,
+                        color: ThemeColors.secondaryTextColor(),
+                      ),
+                    ),
+                  ),
+                  color: ThemeColors.cardColor(),
+                  shape:
+                      RoundedRectangleBorder(borderRadius: kBorderRadius),
+                ),
+              ),
+              SizedBox(
+                height: 80,
+                child: Card(
+                  child: Center(
+                    child: SwitchListTile(
+                      title: Text(
+                        "Use Imperial Units",
+                        style: TextStyle(
+                          color: ThemeColors.primaryTextColor(),
+                        ),
+                      ),
+                      value: useImperial ?? false,
+                      onChanged: (bool value) async {
+                        SharedPrefs.setImperial(value);
+                        useImperial = value;
+                        setState(() {});
+                        Scaffold.of(context).showSnackBar(SnackBar(
+                            content:
+                                Text("Refresh forecast to see changes")));
+                      },
+                      secondary: Icon(
+                        Icons.thermostat_outlined,
+                        color: ThemeColors.secondaryTextColor(),
+                      ),
+                    ),
+                  ),
+                  color: ThemeColors.cardColor(),
+                  shape:
+                      RoundedRectangleBorder(borderRadius: kBorderRadius),
+                ),
+              ),
+              /*
+              SizedBox(
+                height: 80,
+                child: Card(
+                  child: Center(
+                    child: ListTile(
+                      title: Text("Donate"),
+                      onTap: () {
+                      },
+                      leading: Icon(LineIcons.heart_o),
+                    ),
+                  ),
+                  shape:
+                  RoundedRectangleBorder(borderRadius: kBorderRadius),
+                ),
+              ),
+
+               */
+
+              SizedBox(
+                height: 80,
+                child: Card(
+                  child: Center(
+                    child: ListTile(
+                      title: Text(
+                        "About Pluvia Weather",
+                        style: TextStyle(
+                          color: ThemeColors.primaryTextColor(),
+                        ),
+                      ),
+                      onTap: () {
+                        showAboutDialog(
+                          context: context,
+                          applicationVersion: version,
+                          applicationLegalese:
+                              "Pluvia Weather is completely free and open source, and is licensed under GPLv3.",
+                        );
+                      },
+                      leading: Icon(
+                        Icons.more_horiz_outlined,
+                        color: ThemeColors.secondaryTextColor(),
+                      ),
+                    ),
+                  ),
+                  shape:
+                      RoundedRectangleBorder(borderRadius: kBorderRadius),
+                  color: ThemeColors.cardColor(),
+                ),
+              ),
+              SizedBox(
+                height: 12,
+              ),
+              Container(
+                height: 130,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    FlatButton(
+                      onPressed: () async {
+                        const url =
+                            "https://github.com/SpicyChair/pluvia_weather_flutter";
+                        if (await canLaunch(url)) {
+                          await launch(url);
+                        }
+                      },
+                      child: ThemeColors.isDark
+                          ? Image.asset(
+                              "assets/GitHub-Mark-Light-64px.png")
+                          : Image.asset("assets/GitHub-Mark-64px.png"),
+                    ),
+                    SizedBox(
+                      height: 12,
+                    ),
+                    Center(
+                      child: Text(
+                        "View Pluvia Weather on Github",
+                        style: TextStyle(
+                            fontSize: 14,
+                            fontWeight: FontWeight.bold,
+                            color: ThemeColors.primaryTextColor()),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
