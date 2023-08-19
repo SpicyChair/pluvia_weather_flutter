1b00f8227e8c2ab242bad00fd6987e7cb6f300fe
Ethan-Scott Hin <63197802+SpicyChair@users.noreply.github.com>
2023-08-19 09:54:52 +0100

    Fix title text on changelog screen

--- lib/screens/changelog_screen.dart
+++ lib/screens/changelog_screen.dart
@@ -4,32 +4,38 @@ import 'package:flutter_weather/preferences/language.dart';
 import 'package:webview_flutter/webview_flutter.dart';
 
 class ChangelogScreen extends StatelessWidget {
+  final controller = WebViewController()
+    ..setJavaScriptMode(JavaScriptMode.unrestricted)
+    ..loadRequest(
+      Uri.parse(
+          'https://github.com/SpicyChair/pluvia_weather_flutter/releases'),
+    );
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text(
-          Language.getTranslation("weatherRadar"),
-          style: TextStyle(
-            color: Theme.of(context).primaryColorDark,
+        appBar: AppBar(
+          title: Text(
+            Language.getTranslation("changelog"),
+            style: TextStyle(
+              color: Theme.of(context).primaryColorDark,
+            ),
+          ),
+          backgroundColor: Theme.of(context).backgroundColor,
+          centerTitle: true,
+          leading: TextButton(
+            child: Icon(
+              Icons.arrow_back,
+              color: Theme.of(context).primaryColorDark,
+            ),
+            onPressed: () {
+              Navigator.pop(context);
+            },
           ),
         ),
-        backgroundColor: Theme.of(context).backgroundColor,
-        centerTitle: true,
-        leading: TextButton(
-          child: Icon(
-            Icons.arrow_back,
-            color: Theme.of(context).primaryColorDark,
-          ),
-          onPressed: () {
-            Navigator.pop(context);
-          },
-        ),
-      ),
-      body: WebView(
-          initialUrl:
-              'https://github.com/SpicyChair/pluvia_weather_flutter/releases'),
-    );
+        body: WebViewWidget(
+          controller: controller,
+        ));
   }
 }
 
